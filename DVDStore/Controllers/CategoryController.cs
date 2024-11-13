ceaae0966e43652c4d5eefe663234d1c2013b441
jgmalinov <gogotkd24@gmail.com>
2024-11-13 12:18:21 +0200

    Add project files.

---
+++ DVDStore/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using DVDStore.Data;
+using DVDStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DVDStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
