084ac8995c090c35bdeccbbfa138f79167345ab2
jgmalinov <gogotkd24@gmail.com>
2024-11-15 17:43:34 +0200

    Implementing genre listing and creation controllers. Primary styling.

--- DVDStore/Models/Category.cs
+++ DVDStore/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DVDStore.Models
 {
@@ -6,8 +7,13 @@ namespace DVDStore.Models
     {
         [Key]
         public int Id { get; set; }
+        
         [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
+        
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be within the range 1-100.")]
         public int DisplayOrder { get; set; }
     }
 }
